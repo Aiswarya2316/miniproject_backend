e51244f85865f52ac63c92d7453fd0ca43d01070
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-06-26 21:59:51 +0530

    work

--- reverse.py
+++ reverse.py
@@ -4,7 +4,7 @@
 strings_list = ['hello', 'world', 'python', 'heloooo','hello','example']
 print(strings_list)
 for string in strings_list:
-    reversed_string = ''
+    reversed_string = "  "
     for i in string:
         reversed_string =i+reversed_string
     print(reversed_string)
